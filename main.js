dc61d804b83165cf67ec260b5246ca078f0383f3
Rabbi-Hossain <125902257+Rabbi-Hossain@users.noreply.github.com>
2023-02-24 11:40:29 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+//document.getElementById("raja").innerText = 5
+
+let countEl = document.getElementById("count-el")
+let saveEl = document.getElementById('save-El')
+
+console.log(saveEl)
+
+let count = 0
+
+function increment(){
+    count += 1
+    countEl.textContent = count
+}
+
+
+function save(){
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+    countEl.textContent  = 0
+    count = 0
+
+}
+
+
+
